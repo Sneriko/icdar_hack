710d2d28099460d898b8ba0616ecc3dfe8c9185e
Sneriko <erik.lenas@riksarkivet.se>
2026-03-31 10:53:04 +0200

    changed params before run

--- params.py
+++ params.py
@@ -2,7 +2,7 @@
 Hyperparameters for training the Swedish lion.
 """
 
-DATA_PATH = "/mnt/work/GT-Lejonet_HTR_202602"
+DATA_PATH = "/mnt/work/Medeltids_HTR_260330"
 DATA_STOPWORDS = [  # reject samples containing these 'words'
     "??",   # transcriber uncertainty
     "(?)",  # as above, example from JLF: 'dukar, 1 armkläde, 2 harm lijner (?), 1 par toflor,'
@@ -20,7 +20,7 @@ MODEL_IMAGE_SIZE = {"height": 192, "width": 1024}
 MODEL_MAX_LENGTH = 128
 
 TRAIN_LEARNING_RATE = 1e-5
-TRAIN_BATCH_SIZE = 32
+TRAIN_BATCH_SIZE = 16
 TRAIN_MAX_EPOCHS = 50
 TRAIN_EARLY_STOPPING_PATIENCE = 5
 TRAIN_EARLY_STOPPING_MONITOR = "validation_loss"
@@ -31,4 +31,4 @@ AUGMENTATION_PROBABILITY = 0.5
 TORCH_FLOAT32_MATMUL_PRECISION = "medium"
 
 MLFLOW_TRACKING_URI = "https://mlflow.ra.se"
-MLFLOW_EXPERIMENT_NAME = "swedish-lion"
+MLFLOW_EXPERIMENT_NAME = "fornsvensk_htr"
